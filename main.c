8563545b85cc849ec8a1315ef4b7a616da7dbc80
makoto hasunuma <makot@codez.jp>
2023-05-18 21:15:20 +0900

    baremetal coding

--- main.c
+++ main.c
@@ -1,61 +1,26 @@
 #define F_CPU 16000000UL
-
-#include "sub.h"
-
-//#include "unolib.h"
-//#include "pinMode.h"
-//#include "digitalWrite.h"
 #include "util/delay.h"
 
-#include <stdbool.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/sleep.h>
+#define MY_DDRB (*(volatile uint8_t *)(0x24))
+#define MY_PORTB (*(volatile uint8_t *)(0x25))
+#define MY_PORTB5 5
 
 
-int main(void)
-{
-    int ret;
-    
-    ret = sub( );
-    return ret; 
+void setup( void ){
+    MY_DDRB |= (1 << MY_PORTB5);
+    MY_PORTB |= (1 << MY_PORTB5);
 }
 
-
-bool is_bit_on( void )
-{
-    return true;
-}
-
-
-int _main(void)
-{
-/*
-    DDRB |= (_BV(PORTB5));\
-    PORTB |= (_BV(PORTB5));\
-*/
-    DDRB |= (1 << PORTB5);
-    PORTB |= (1 << PORTB5);
+int main( void ) {
+    setup();
     
-    while( 1 )
-    {
-        if ( is_bit_on( ) == true )
-        {
-/*
- *             _delay_ms(1000);\
- *            PORTB &= ~(_BV(PORTB5));
- */
+    while( 1 ){
+        MY_PORTB = (1 << MY_PORTB5);
+        _delay_ms(10);
 
-            PORTB = (1 << PORTB5);
-            _delay_ms(33);
-            PORTB &= ~(1 << PORTB5);
-            _delay_ms(100);
-            PORTB = (1 << PORTB5);
-            _delay_ms(33);
-
-            PORTB &= ~(1 << PORTB5);
-            _delay_ms(980);
-        }
+        MY_PORTB &= ~(1 << MY_PORTB5);
+        _delay_ms(2990);        
     }
-    return 0; 
+    return(1);
 }
+
