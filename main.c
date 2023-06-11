38f25addb3f1c4f83126643571517eef1986e143
makoto hasunuma <makot@codez.jp>
2023-06-11 15:05:32 +0900

    for day2

--- main.c
+++ main.c
@@ -5,17 +5,21 @@
 #define MY_DDRB (*(volatile unsigned char *)(0x24))
 #define MY_PORTB (*(volatile unsigned char *)(0x25))
 
-int main( void ) {
-    // setup())
-    MY_DDRB |= (1 << 5);  // MY_DDRB = 0b00100000;
-  
-    while( 1 ){
-        // Loop()
-        MY_PORTB |= (1 << 5);
-        _delay_ms(1500);
 
-        MY_PORTB &= ~(1 << 5);
-        _delay_ms(1500);        
+void setup( void ) {  // setup()
+    MY_DDRB |= (1 << 5);  // set portB bit6 OUTPUT -> 0b00100000
+}
+
+
+int main( void ) {
+    setup();
+
+    while( 1 ){  // loop
+        MY_PORTB |= (1 << 5);  // set portB bit6
+        _delay_ms(50);
+
+        MY_PORTB &= ~(1 << 5);  // clear portB bit6
+        _delay_ms(950);        
     }
     return 0;
 }
--- upload.bat
+++ upload.bat
@@ -1,31 +1,60 @@
 echo off
 
-rem echo UserProfile %UserProfile%
-rem echo UserName %UserName%
-rem echo AppData %AppData%
-rem echo LocalAppData %LocalAppData%
-rem echo CurrentDirectory %CD%
-rem echo CD %CD%
-rem echo SELF %0
+rem echo ====== for testing ======
+rem echo ProgramFiles(x86)  %ProgramFiles(x86)%
+rem echo ProgramFiles       %ProgramFiles%
+rem echo LocalAppData       %LocalAppData%
+rem echo CurrentDirectory   %CD%
+rem echo UserProfile    %UserProfile%
+rem echo UserName       %UserName%
+rem echo AppData        %AppData%
+rem echo CD             %CD%
+rem echo SELF           %0
+rem echo ====== for testing ======
 
 rem https://stackoverflow.com/questions/17279114/split-path-and-take-last-folder-name-in-batch-script
 set MYDIR=%CD%\
 if "%MYDIR:~-1%" == "\" set "MYDIR1=%MYDIR:~0,-1%"
 for %%f in ("%MYDIR1%") do set "FOLDER=%%~nxf"
-echo FOLDER %FOLDER%
+echo PROJ   %FOLDER%
 
-set TOOL=\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17
-set EXE=%LocalAppData%%TOOL%/bin/avrdude.exe
-set CONF=%LocalAppData%%TOOL%/etc/avrdude.conf
-set HEX=%CD%\dist\default\production/%FOLDER%.production.hex
+set TOOL=\bin\avrdude.exe
+set CONF=\etc\avrdude.conf
+set ROOT=%ProgramFiles(x86)%\Arduino\hardware\tools\avr
 
-echo "---------------------"
-echo EXE  %EXE%
-echo CONF %CONF%
-echo HEX  %HEX%
-echo "---------------------"
+if not exist "%ROOT%%TOOL%" (
+    echo not found TOOL "%ROOT%"
+    set ROOT=%LocalAppData%\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17
+    echo change ROOT to "%ROOT%"
+)
+echo ROOT   %ROOT%
 
-set CMD=%EXE% -C%CONF% -v -patmega328p -carduino -PCOM3 -b115200 -D -Uflash:w:%HEX%:i
-echo CMD %CMD%
-%CMD%
+set HEX=%CD%\dist\default\production\%FOLDER%.production.hex
+if not exist "%HEX%" goto not_found_hex
+echo HEX    %HEX%
+
+set CMD="%ROOT%%TOOL%" -C "%ROOT%%CONF%" -patmega328p -carduino -PCOM3 -b115200 -D -U flash:w:%HEX%:i
+echo CMD        %CMD%
+%CMD% -v
+goto :OK
+
+
+
+:not_found_exe
+echo ERROR ERROR ERROR
+echo not found avrdude.exe
+echo please install avrdude.exe with Arduino IDE.
+goto :EOF
+
+:not_found_hex
+echo ERROR ERROR ERROR
+echo not found hex file
+echo please re-build with **NOT DEBUG** mode.
+goto :EOF
+
+:OK
+echo OK done.
+goto :EOF
+
+:EOF
 pause
